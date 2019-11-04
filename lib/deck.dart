c139f9f210d46422d772a7d138592d15b05a9a88
Christian Niekler <christian.niekler@student.fhws.de>
2019-11-04 19:59:13 +0100

    createDeck() implemented

--- lib/deck.dart
+++ lib/deck.dart
@@ -1,17 +1,30 @@
 import 'package:wizard2/card.dart';
+import 'package:wizard2/types.dart';
 
 class Deck {
-  List<Card> cards = new List(60);
+  List<Card> deck = new List(60);
   void distribution() {}
   void shuffleCards() {}
 }
 
 Deck createDeck() {
   // clubs (♣), diamonds (♦), hearts (♥) and spades (♠)
-  Deck output;
+  Deck output = new Deck();
+  int index = 0;
   // first loop for the four different types
   for (int i = 0; i < 4; i++) {
     // second loop for the fourteen different cards
-    for (int j = 0; j < 14; j++) {}
+    for (int j = 0; j < 15; j++) {
+      //   Card testCard = new Card(cardTypes.SPADE, 4)
+      if (j == 0) {
+        output.deck[index] = new Card(cardTypes.values[4], j);
+      } else if (j == 14) {
+        output.deck[index] = new Card(cardTypes.values[5], j);
+      } else {
+        output.deck[index] = new Card(cardTypes.values[i], j);
+      }
+      index++;
+    }
   }
+  return output;
 }
