1a98e173d12c2b2b31ed736ef3dbc252ae6a70c2
Julian <julian.hanke@student.fhws.de>
2019-11-05 21:48:11 +0100

    overridden method playCard in class ki

--- lib/ki.dart
+++ lib/ki.dart
@@ -6,6 +6,12 @@ import 'package:wizard2/deck.dart';
 import 'package:wizard2/cardTypes.dart';
 
 class Ki extends Player{
+  @override
+  Card playCard(int pick) {
+    Card temp = this.handCards[0];
+    handCards.removeAt(0);
+    return temp;
+  }
 
 
   //karte legen
@@ -22,7 +28,7 @@ class Ki extends Player{
   //todo erste bet erstmal immer 1
   //todo zweite bet alle Karten größer gleich 10 ist Anzahl der bet -> wenn nicht möglich zu legen auf Grund der Logik, dann eine weniger wetten
   //todo Wahrscheinlichkeitsberechnung für bessere Karten, als alle Anderen
-  //todo an Hand der Wahrscheinlichkeit die dritte bet 
+  //todo an Hand der Wahrscheinlichkeit die dritte bet
 
   //todo Tests für KI
 
