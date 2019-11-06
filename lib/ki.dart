da37155e9f04f8f01873e32de1924d907447fe48
Julian <julian.hanke@student.fhws.de>
2019-11-06 22:34:32 +0100

    ki only picks from playable Handcards

--- lib/ki.dart
+++ lib/ki.dart
@@ -5,15 +5,25 @@ import 'package:wizard2/card.dart';
 import 'package:wizard2/deck.dart';
 import 'package:wizard2/cardTypes.dart';
 import 'dart:math' show Random;
+import 'package:wizard2/round.dart';
 
 class Ki extends Player {
   @override
   Card playCard(int pick) {
     var rndm = new Random();
-    var numpick = rndm.nextInt(pick);
-    //print(numpick);   //Test if it's random
-    Card temp = this.handCards[numpick]; //used for play random card
-    handCards.removeAt(numpick);
+
+
+    //here between all handcards is chosen
+    //var numpick = rndm.nextInt(pick);
+    ////print(numpick);   //Test if it's random
+    //Card temp = this.handCards[numpick]; //used for play random card
+    //handCards.removeAt(numpick);
+
+    this.creatingPlayableHandCardsList();
+    var numpick = rndm.nextInt(playableHandCards.length);
+    Card temp = this.playableHandCards[numpick]; //used for play random card
+    handCards.remove(this.playableHandCards[numpick]);
+
     //Card temp = this.handCards[0];      //used for play first card
     //handCards.removeAt(0);
     return temp;
@@ -27,7 +37,7 @@ class Ki extends Player {
 
   //karte legen
   //both done erste KI mit random oder erster Kart
-  //todo zweite KI mit erster (oder random) legbarer Karte
+  //done zweite KI mit erster (oder random) legbarer Karte
   //todo dritte KI mit bester legbarer Karte
   //todo Wahrscheinlichkeitsberechnung für Gewinn des Stichs
   //todo vierte KI beste legbare Kartodote, wenn Wahrscheinlichkeit für Stich größer 0,75
