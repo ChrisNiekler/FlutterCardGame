53f75aad65ffd90d51145c51a889b541f0e3afb0
Christian Niekler <christian.niekler@student.fhws.de>
2019-11-13 20:05:37 +0100

    added more structure to test samples

--- test/player_test.dart
+++ test/player_test.dart
@@ -1,12 +1,23 @@
 import 'package:test/test.dart';
 
 void main() {
-  group('Game:', () {
-    test('Something', () {
-      fail('not yet implemented');
-    });
-    test('Something esle', () {
-      fail('not yet implemented');
-    });
-  });
+  group('ALL:', () {
+    group('group1:', () {
+      test('Something', () {
+        fail('not yet implemented');
+      });
+      test('Something esle', () {
+        fail('not yet implemented');
+      });
+    }); // end group1
+
+    group('group1:', () {
+      test('Something', () {
+        fail('not yet implemented');
+      });
+      test('Something esle', () {
+        fail('not yet implemented');
+      });
+    }); // end group2
+  }); // end of ALL
 }
