f3441608c9b525c92a4836b32063fc44aef9abda
Christian Niekler <christian.niekler@student.fhws.de>
2019-11-13 15:50:16 +0100

    created first simple unit tests

--- test/cardTest.dart
+++ test/cardTest.dart
@@ -1,21 +1,36 @@
 import 'package:wizard2/cardTypes.dart';
 import 'package:wizard2/card.dart';
-//todo import the test package and implement the tests
+import 'package:test/test.dart';
 
-void cardTest() {
-  Card testCard = new Card(cardTypes.SPADE, 4);
-  print(testCard.card);
-  Card testCard2 = new Card(cardTypes.DIAMOND, 5);
-  print(testCard2.card);
-  Card testCard3 = new Card(cardTypes.HEART, 6);
-  print(testCard3.card);
-  Card testCard4 = new Card(cardTypes.CLUB, 7);
-  print(testCard4.card);
-  Card testCard5 = new Card(cardTypes.WIZARD, 14);
-  print(testCard5.card);
-  Card testCard6 = new Card(cardTypes.JESTER, 0);
-  print(testCard6.card);
+//todo import the test package and implement the tests
+// https://flutter.dev/docs/cookbook/testing/unit/introduction
+void main() {
+  group('Cards:', () {
+    test('Card is created', () {
+      Card card = new Card(cardTypes.WIZARD, 14);
+      expect(card, isNotNull);
+    });
+    test('Card has expected name', () {
+      Card card = new Card(cardTypes.WIZARD, 14);
+      expect(card.card, 'WIZARD  (🧙)');
+    });
+  });
 }
+
+//void cardTest() {
+//  Card testCard = new Card(cardTypes.SPADE, 4);
+//  print(testCard.card);
+//  Card testCard2 = new Card(cardTypes.DIAMOND, 5);
+//  print(testCard2.card);
+//  Card testCard3 = new Card(cardTypes.HEART, 6);
+//  print(testCard3.card);
+//  Card testCard4 = new Card(cardTypes.CLUB, 7);
+//  print(testCard4.card);
+//  Card testCard5 = new Card(cardTypes.WIZARD, 14);
+//  print(testCard5.card);
+//  Card testCard6 = new Card(cardTypes.JESTER, 0);
+//  print(testCard6.card);
+//}
 
 // todo implement a test for testing the card distribution
 //// prints out all the cards of all the players to the console
