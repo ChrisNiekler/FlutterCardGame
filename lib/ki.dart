ad2d8bd831a51ce6a1668cff1279e253826fceb2
Julian <julian.hanke@student.fhws.de>
2019-11-14 21:37:50 +0100

    code clean up in class ki

--- lib/ki.dart
+++ lib/ki.dart
@@ -3,6 +3,8 @@ import 'package:wizard/card.dart';
 import 'package:wizard/cardType.dart';
 import 'dart:math' show Random;
 
+//todo Tests für KI
+
 class Ki extends Player {
   Ki(name, id) {
     this.ai = true;
@@ -14,6 +16,7 @@ class Ki extends Player {
   Card playCard(int pick, {CardType trump, Card foe}) {
     //1. here it is chosen between all handcards
     if (trump == null) {
+      //todo improve (when there is no trump)
       Card temp = findBestCard();
       handCards.remove(temp);
       return temp;
@@ -21,23 +24,17 @@ class Ki extends Player {
       //todo if no card is played yet (improve this)
       //2. here it is chosen between all playable handcards
       pick = Random().nextInt(playableHandCards.length);
-      //Card temp = this.playableHandCards[numpick]; //used for play random card
       return handCards.removeAt(pick);
     } else {
       return playCardAI(foe, trump);
     }
-
-    //return temp;
   }
 
-// @override
   Card playCardAI(Card foe, CardType trump) {
     //3. here play best or worst Card -> at the  moment problem caching value of the best played card and the trump
-    //todo value of best playedcard in round F4N
+    //todo get more intelligent (Wahrscheinlichkeiten, ...)
     Card bestCard = findBestCard();
     Card worstCard = findWorstCard();
-//    Card tump = Round.giveTrumpCard();
-//    Card nowbest = bestPlayedCardYet();
     if (bestCard == bestCard.compare(foe, trump) &&
         tricks < bet &&
         foe.cardType != CardType.WIZARD) {
@@ -52,9 +49,10 @@ class Ki extends Player {
   }
 
   //karte legen
-  //todo done erste KI mit random oder erster Kart F4N
-  //todo zweite KI mit erster (oder random) legbarer Karte F4N
-  //todo dritte KI mit bester legbarer Karte F4N
+  //todo DONE erste KI mit random oder erster Karte
+  //todo DONE zweite KI mit erster (oder random) legbarer Karte
+  //todo DONE dritte KI mit bester legbarer Karte
+
   //todo Wahrscheinlichkeitsberechnung für Gewinn des Stichs
   //todo vierte KI beste legbare Kartodote, wenn Wahrscheinlichkeit für Stich größer 0,75
   //todo fünfte KI beste legbare Karte, wenn hohe Wahrscheinlichkeit für Stich und auch überhaupt noch ein Stich benötigt
@@ -70,62 +68,40 @@ class Ki extends Player {
       else if (handCards[i].value > 11) bet++;
     }
     check = bet + betsNumber;
-    //todo cannot bet more than the possible handcards  -> NOT ALLOWED TO bet -1 F4N
-    if (this.lastPlayer && check == round && bet == 0)
-      bet++;
-    else if (this.lastPlayer && check == round) bet--;
+    if (this.lastPlayer && check == round) {
+      if (bet == 0)
+        bet++;
+      else
+        bet--;
+    }
     print('$name bet he/she wins $bet tricks!');
   }
 
   //wetten
-  //todo erste bet erstmal immer 1 F4N
-  //todo zweite bet alle Karten größer gleich 10 ist Anzahl der bet F4N -> wenn nicht möglich zu legen auf Grund der Logik, dann eine weniger wetten F4N
+  //todo DONE erste bet erstmal immer 1
+  //todo DONE zweite bet alle Karten größer gleich 10 ist Anzahl der bet -> wenn nicht möglich zu legen auf Grund der Logik, dann eine weniger wetten
+
   //todo Wahrscheinlichkeitsberechnung für bessere Karten, als alle Anderen
   //todo an Hand der Wahrscheinlichkeit die dritte bet
 
-  //todo Tests für KI
-
   Card findBestCard() {
-    //find algorithm for finding best card of the Hand  (and worst)
-    //then only if trick can be made
+    //todo maybe check if there is a check with trump and not trump needed
     Card bestCard = this.playableHandCards[0];
     for (int i = 1; i < playableHandCards.length; i++) {
       if (bestCard.value < playableHandCards[i].value)
         bestCard = playableHandCards[i];
     }
     return bestCard;
-    //todo next step compare() auf eigene Handkarten (trump and highest card needed)
-    //for(int i = 0; i < playableHandCards.length; i++) {
-    //  playableHandCards.compareHandCards(foe, turmp)
-  }
-
-  int findIndexBestCard() {
-    //no usage
-    Card bestCard = this.playableHandCards[0];
-    int x = 0;
-    for (int i = 1; i < playableHandCards.length; i++) {
-      if (bestCard.value < playableHandCards[i].value) x = i;
-    }
-    return x;
   }
 
   Card findWorstCard() {
+    //todo maybe check if there is a check with trump and not trump needed
     Card worstCard = this.playableHandCards[0];
     for (int i = 1; i < playableHandCards.length; i++) {
       if (worstCard.value > playableHandCards[i].value)
         worstCard = playableHandCards[i];
     }
     return worstCard;
-  }
-
-  int findIndexWorstCard() {
-    //no usage
-    Card worstCard = this.playableHandCards[0];
-    int x = 0;
-    for (int i = 1; i < playableHandCards.length; i++) {
-      if (worstCard.value > playableHandCards[i].value) x = i;
-    }
-    return x;
   }
 
 //todo if wizard is trump (pick trump)
