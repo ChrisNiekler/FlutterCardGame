707b0af33a43a88677ebc54d25f85e06764c9212
Tran, Trong Duc <trongduc.tran@student.fhws.de>
2019-11-15 23:56:17 +0100

    added a List to create the amount of cards needed

--- lib/playerTemplateGUI.dart
+++ lib/playerTemplateGUI.dart
@@ -5,6 +5,7 @@ class PlayerTemplate extends StatelessWidget {
   final String playerName;
 
   PlayerTemplate({this.playerName});
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -13,17 +14,17 @@ class PlayerTemplate extends StatelessWidget {
         height: 500.0,
         width: 100.0,
         color: Colors.grey,
-        child: Column(
-          children: <Widget>[
-            Text("$playerName"),
-            CreateCardImageBack(Offset(100.0, 100.0)),
-            CreateCardImageBack(Offset(100.0, 100.0)),
-            CreateCardImageBack(Offset(100.0, 100.0)),
-
-          ],
-        ),
+        child: Column(children: cardsPerRound()),
       ),
     );
   }
 
+  int round = 2;
+
+  List<CreateCardImageBack> cardsPerRound() {
+    List<CreateCardImageBack> cardList = [];
+    for (int i = 0; i < round; i++)
+      cardList.add(CreateCardImageBack(Offset(100.0, 100.0)));
+    return cardList;
+  }
 }
