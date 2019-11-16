d345fdef51320da98c0f08af18754153ffd88197
Christian Niekler <christian.niekler@student.fhws.de>
2019-11-16 14:39:51 +0100

    added playing_card.dart

---
+++ lib/playing_card.dart
@@ -0,0 +1,19 @@
+import 'package:wizard/card.dart' as gamecard;
+
+import 'card.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class PlayingCard extends StatefulWidget {
+  @override
+  _PlayingCardState createState() {
+    _PlayingCardState();
+  }
+}
+
+class _PlayingCardState extends State<PlayingCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
