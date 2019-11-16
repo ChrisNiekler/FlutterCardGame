d8cf2c8df05f722798c2d8542dd4fba97ebd77c8
vasi <vasileios.tsanakas.1@student.fhws.de>
2019-11-16 16:51:57 +0100

    Random AI

---
+++ lib/ai.dart
@@ -0,0 +1,46 @@
+import 'package:wizard/player.dart';
+import 'package:wizard/card.dart';
+import 'cardType.dart';
+import 'dart:math' show Random;
+
+//todo Tests für AI
+
+class Ai extends Player { //Klasse Ai erbt von Klasse Player//
+  Ai(name,id){
+    this.name = name; //name übernehmen//
+    this.id = id; //id übernehmen//
+    this.ai = true; //ai auf true setzen//
+  }
+
+  Card playCardAI(Card foe, CardType Trump){
+    int pick = Random().nextInt(playableHandCards.length);
+    return handCards.removeAt(pick);
+  }
+
+  Card findCard() {
+    int temp = Random().nextInt(playableHandCards.length);
+    Card findCard = playableHandCards[temp];
+    return findCard;
+  }
+
+  @override
+  Card playCard(int pick, {CardType trump, Card foe}){
+    if (trump == null){
+      Card temp = findCard();
+      handCards.remove(temp);
+      return temp;
+    } else if (foe == null){
+      pick = Random().nextInt(playableHandCards.length);
+      return handCards.removeAt(pick);
+    } else {
+      return playCardAI(foe , trump);
+    }
+  }
+
+  @override
+  void putBet(int round, int betsNumber, {CardType trump, String testValue}) {
+    this.bet = Random().nextInt(handCards.length);
+    print('$name bet he/she wins $bet tricks!');
+  }
+
+}
