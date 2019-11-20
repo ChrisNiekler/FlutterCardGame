3953758ac62e4c0862e80d991f1caacc149769c9
Tran, Trong Duc <trongduc.tran@student.fhws.de>
2019-11-20 23:54:42 +0100

    deleted height and width of the container and rotated the cards

--- lib/playerTemplateGUI.dart
+++ lib/playerTemplateGUI.dart
@@ -10,34 +10,43 @@ class PlayerTemplate extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       flex: 4,
-      child: Row(
-        children: <Widget>[Container(
-          height: 500.0,
-          width: 100.0,
+      child: Row(children: <Widget>[
+        Container(
           color: Colors.grey,
           child: Row(
             children: <Widget>[
-              Column(mainAxisAlignment: MainAxisAlignment.center,children: cardsPerRoundFoe()),
-
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: cardsPerRoundFoe()),
             ],
           ),
-        ),Container(width: 60.0,height:40.0,child: Image.asset("images/cards/1hearts.png"),)]
-      ),
+        ),
+        Container(
+          width: 60.0,
+          height: 40.0,
+          child: RotatedBox(
+              quarterTurns: 5, child: Image.asset("images/cards/1hearts.png")),
+        )
+      ]),
     );
   }
 
-  int round = 4;
+  int round = 2;
 
   List<CreateCardImageBack> cardsPerRoundFoe() {
     List<CreateCardImageBack> cardList = [];
-    for (int i = 0; i < round; i++)
-      cardList.add(CreateCardImageBack());
+    for (int i = 0; i < round; i++) cardList.add(CreateCardImageBack());
     return cardList;
   }
+
   List<CreateCardImage> cardsPerRoundPlayer(String cardID) {
+    List<String> cardID = [];
+    cardID[0] = "1hearts";
+    cardID[1] = "2diamonds";
+    cardID[2] = "5clubs";
+    cardID[3] = "6spades";
     List<CreateCardImage> cardList = [];
-    for (int i = 0; i < round; i++)
-      cardList.add(CreateCardImage(cardID));
+    for (int i = 0; i < round; i++) cardList.add(CreateCardImage(cardID[i]));
     return cardList;
   }
 }
