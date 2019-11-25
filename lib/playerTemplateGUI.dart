0e8a4a128b650f63db1d00ff45589935f485fb04
Tran, Trong Duc <trongduc.tran@student.fhws.de>
2019-11-26 00:30:24 +0100

    refactored this class from Stateless to Stateful

--- lib/playerTemplateGUI.dart
+++ lib/playerTemplateGUI.dart
@@ -1,57 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:wizard/playing_card.dart';
 import 'gamepage.dart';
 
-class PlayerTemplate extends StatelessWidget {
+class PlayerTemplate extends StatefulWidget {
   final String playerName;
   final bool isLeft;
 
   PlayerTemplate({this.playerName, this.isLeft});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: isLeft
-          ? Expanded(
-              flex: 1,
-              child: Row(children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: cardsPerRoundFoe()),
-                  ],
-                ),
-                Container(
-                  width: 60.0,
-                  height: 40.0,
-                  child: RotatedBox(
-                      quarterTurns: 5,
-                      child: Image.asset("images/cards/1hearts.png")),
-                )
-              ]))
-          : Expanded(
-          flex: 6,
-          child: Row(mainAxisAlignment: MainAxisAlignment.end,children: <Widget>[
-            Container(
-              width: 60.0,
-              height: 40.0,
-              child: RotatedBox(
-                  quarterTurns: 5,
-                  child: Image.asset("images/cards/1hearts.png")),
-            ),Row(
-              children: <Widget>[
-                Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: cardsPerRoundFoe()),
-              ],
-            )
-
-          ]))
-    );
-  }
 
   int round = 2;
 
@@ -61,14 +17,69 @@ class PlayerTemplate extends StatelessWidget {
     return cardList;
   }
 
-  List<CreateCardImage> cardsPerRoundPlayer(String cardID) {
+  List<PlayingCard> cardsPerRoundPlayer() {
     List<String> cardID = [];
     cardID[0] = "1hearts";
     cardID[1] = "2diamonds";
     cardID[2] = "5clubs";
-    cardID[3] = "6spades";
-    List<CreateCardImage> cardList = [];
-    for (int i = 0; i < round; i++) cardList.add(CreateCardImage(cardID[i]));
+    cardID[3] = "10spades";
+    List<PlayingCard> cardList = [];
+    for (int i = 0; i < round; i++) cardList.add(PlayingCard(cardID[i]));
     return cardList;
+  }
+
+  void removePlayedCard(String card) {
+    cardsPerRoundPlayer().remove(card);
+  }
+
+  @override
+  _PlayerTemplateState createState() => _PlayerTemplateState();
+}
+
+class _PlayerTemplateState extends State<PlayerTemplate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: widget.isLeft
+            ? Expanded(
+                flex: 1,
+                child: Row(children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: widget.cardsPerRoundFoe()),
+                    ],
+                  ),
+                  Container(
+                    width: 60.0,
+                    height: 40.0,
+                    child: RotatedBox(
+                        quarterTurns: 5,
+                        child: Image.asset("images/cards/1hearts.png")),
+                  )
+                ]))
+            : Expanded(
+                flex: 6,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Container(
+                        width: 60.0,
+                        height: 40.0,
+                        child: RotatedBox(
+                            quarterTurns: 5,
+                            child: Image.asset("images/cards/1hearts.png")),
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: widget.cardsPerRoundFoe()),
+                        ],
+                      )
+                    ])));
   }
 }
