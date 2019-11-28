1bcd0edfdce397a1cdb03debac72bebfff6abf01
Tran, Trong Duc <trongduc.tran@student.fhws.de>
2019-11-28 12:35:17 +0100

    move code from main to own files

---
+++ lib/ui/player_BottomTable.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:wizard/ui/cardDropField.dart';
+import 'package:wizard/ui/playing_card.dart';
+import 'package:wizard/ui/playerTemplateGUI.dart';
+//this is for the user able to see his cards and play from here
+class BottomPart extends StatelessWidget {
+ BottomPart({this.cardID});
+  final String cardID;
+  final List<PlayingCard> cardList = [
+    PlayingCard('2diamonds'),
+    PlayingCard('3diamonds'),
+    PlayingCard('5spades'),
+   PlayingCard('10hearts'),
+  ];
+
+
+final PlayerTemplate player = PlayerTemplate();
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      color: Colors.grey,
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 3 / 10 - 25.0,
+      child: Column(
+        children: <Widget>[
+          CardDropField(cardID),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: cardList),
+          Text("Playername")
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/ui/trump_MidTable.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+//for the trump, still need to extend this for the 6th player
+class MidPart extends StatefulWidget {
+  @override
+  _MidPartState createState() => _MidPartState();
+}
+
+class _MidPartState extends State<MidPart> {
+  String cardID;
+  String cardTrump;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints.tightForFinite(
+        height: MediaQuery.of(context).size.height * 7 / 10,
+        width: MediaQuery.of(context).size.width * 1 / 5,
+      ),
+      color: Colors.grey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          DragTarget(
+            builder: (context, List<String> acceptedCards, rejectedCards) {
+              print(acceptedCards);
+              return Container(
+                height: 150.0,
+                width: 100.0,
+                color: Colors.white70,
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Trump',
+                      style: TextStyle(color: Colors.white, fontSize: 22.0),
+                    ),
+                    //cardID == null ? Container() : CreateCardImage('$cardID'),
+                    cardID == null
+                        ? Container()
+                        : Container(
+                      width: 40.0,
+                      height: 80.0,
+                      child: Image.asset("images/cards/$cardID.png"),
+                    )
+
+                    //Text("$cardID"),
+                  ],
+                ),
+              );
+            },
+            onWillAccept: (data) {
+              return true;
+            },
+            onAccept: (data) {
+              setState(() {
+                cardID = data;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
