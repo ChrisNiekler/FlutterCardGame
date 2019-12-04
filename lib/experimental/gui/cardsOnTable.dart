90d14efbf2496a4a04ce42a6dcc9aa990b948690
Christian Niekler <christian.niekler@student.fhws.de>
2019-12-04 15:46:54 +0100

    cardsOnTable

--- lib/experimental/gui/cardsOnTable.dart
+++ lib/experimental/gui/cardsOnTable.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:wizard/logic/card.dart' as logic;
 
-Widget cardOnTable(Color bgcolor, logic.Card playerField) {
-  return Expanded(
-    child: Container(
-      color: bgcolor,
-      child: Center(
-          child: (playerField != null) ? playedCard(playerField) : Container()),
-    ),
-  );
+Widget cardOnTable(logic.Card playerField, {bool trumpCard}) {
+  Widget _out_widget;
+  if (trumpCard == null) {
+    return Expanded(
+      child: Container(
+        child: Center(
+            child:
+                (playerField != null) ? _playedCard(playerField) : Container()),
+      ),
+    );
+  }
 }
 
-Widget playedCard(logic.Card tCard) {
+Widget _content() {}
+
+Widget _playedCard(logic.Card tCard) {
   return Padding(
     padding: const EdgeInsets.all(8.0),
     child: tCard.playerCardsWidget(),
