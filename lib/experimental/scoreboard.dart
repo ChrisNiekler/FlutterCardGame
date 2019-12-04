07811581cf359b3beda1f64631088dd54ac306e7
Julian <julian.hanke@student.fhws.de>
2019-12-04 14:11:05 +0100

    outsourced Scoreboard

---
+++ lib/experimental/scoreboard.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+
+Widget scoretable() {
+  return AlertDialog(
+    content: Column(
+      children: <Widget>[
+        Container(
+          child: Text("hallo"),
+        ),
+      ],
+    ),
+  );
+}
