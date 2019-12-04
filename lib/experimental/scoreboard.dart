fa067effb71fe0e14899411f0b49015d623a5fa7
Julian <julian.hanke@student.fhws.de>
2019-12-04 15:43:27 +0100

    refactored Scoreboard

--- lib/experimental/scoreboard.dart
+++ lib/experimental/scoreboard.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
-Widget scoretable() {
+AlertDialog scoretable(BuildContext context) {
   return AlertDialog(
-    content: Column(
-      children: <Widget>[
-        Container(
-          child: Text("hallo"),
-        ),
-      ],
+    content: Container(
+      height: 70,
+      padding: const EdgeInsets.all(20),
+      margin: const EdgeInsets.only(top: 5),
+      decoration: BoxDecoration(
+//          color: Colors.white,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.grey[300],
+            blurRadius: 3,
+            spreadRadius: 6,
+          )
+        ],
+      ),
+      child: Text("Hier sollte das Scoreboard sein!!!"),
     ),
   );
 }
