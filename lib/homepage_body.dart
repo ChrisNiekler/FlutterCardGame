ba06c7b99b23c9fec435c0c903e68ecaca57fa19
Christian Niekler <christian.niekler@student.fhws.de>
2019-12-09 16:03:07 +0100

    removed

---
+++ lib/homepage_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class HomePageBody extends StatefulWidget {
+  @override
+  HomePageBodyState createState() => new HomePageBodyState();
+}
+
+class HomePageBodyState extends State<HomePageBody> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
